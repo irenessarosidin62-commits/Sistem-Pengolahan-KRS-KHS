9553388869d9db878a722de1d91da5ef4c7cfb0a
reyhan07798-cpu <reyhan07798@gmail.com>
2026-03-16 22:11:08 +0700

    Ni akuu udhh

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
+})->middleware('auth');
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
 });
+
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
