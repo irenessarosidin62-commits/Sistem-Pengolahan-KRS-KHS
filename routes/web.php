845a8f3345d2ffb0955161b6138a32a765a8c334
reyhan07798-cpu <reyhan07798@gmail.com>
2026-03-16 23:26:56 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -1,62 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ListBarangController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardKrsController;
 
-//Route::get('/', function () {
-  //  return view('welcome');
-//});
-
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->middleware('auth');
 Route::get('/contact', [HomeController::class, 'contact']);
-
-// routes/web.php
 
 Route::get('/welcome', function () {
    return view('welcome');
-
 });
-
-// routes/web.php
 
 Route::get('/user/{id}', function ($id) {
     return 'User dengan ID ' . $id;
 });
 
-// routes/web.php
-
 Route::prefix('admin')->group(function () {
-
     Route::get('/dashboard', function () {
         return 'Admin Dashboard';
     });
-
     Route::get('/users', function () {
         return 'Admin Users';
     });
-
 });
-
-// Route::get('/listbarang/{id}/{nama}', function($id, $nama){
-//     return view('list_barang', compact('id', 'nama'));
-// });
 
 Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
-=======
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\DashboardKrsController;   
-
-Route::get('/', function () {
-    return view('welcome');
-})->middleware('auth');
-
-Route::middleware('guest')->group(function () {
-    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-    Route::get('/dashboard', [DashboardKrsController::class, 'index'])->name('dashboard');
-    Route::post('/login', [LoginController::class, 'login']);
-});
-
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
->>>>>>> 8c6781041af69ef329f52da898fdc0ad376d2c52
